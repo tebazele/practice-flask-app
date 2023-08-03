00e6b466b6e033858e30a74faa3407ef8e1b81e3
Jeanne <tebazele@gmail.com>
2023-08-03 16:32:34 -0600

    last commit

--- CraftRanker.client/src/pages/HomePage.vue
+++ CraftRanker.client/src/pages/HomePage.vue
@@ -37,18 +37,83 @@
     </div>
   </section>
   <hr />
-  <section class="row">
+  <section class="row justify-content-center">
     <div class="col-12">
       <h2 class="text-center">What Other Etsy Sellers Are Saying:</h2>
-      <div class="card p-3 m-5 pos-rel bg-white">
-        <img src="../assets/img/doubleQuote.png" class="quote-art" />
-        <div>
-          <p>My shop improved immediately.A highly recommended resource!</p>
-          <h6>WoodenGiftCo</h6>
-
+    </div>
+    <!-- SECTION reviews -->
+    <!-- STUB 1 -->
+    <div class="col-10">
+      <div class="my-2 mx-5 px-5 py-3 border rounded bg-white row align-items-baseline">
+        <img src="../assets/img/doubleQuote.png" class="quote-art col-4" />
+        <div class="col-10 ">
+          <p class="mb-2">My shop improved immediately.A highly recommended resource!</p>
+          <h6 class="text-info mt-0 text-end">~ WoodenGiftCo</h6>
         </div>
       </div>
+
     </div>
+    <!-- STUB 2 -->
+    <div class="col-10">
+      <div class="my-2 mx-5 px-5 py-3 border rounded bg-white row align-items-baseline">
+        <img src="../assets/img/doubleQuote.png" class="quote-art col-4" />
+        <div class="col-8 ">
+          <p class="mb-2">I've been away from selling on Etsy for over seven years, and I knew coming back, I was walking
+            into a completely different selling experience. I had no desire to overwhelm myself with internet rabbit holes
+            and google searches trying to figure out what changed and what that meant for me ... I highly recommend this
+            to any new sellers or anyone coming back to Etsy after a few years away. I wish I would have had it years ago
+            when I tried to sell the first time.</p>
+          <h6 class="text-info mt-0 text-end">~ Sue S.</h6>
+        </div>
+      </div>
+
+    </div>
+    <!-- STUB 3 -->
+    <div class="col-10">
+      <div class="my-2 mx-5 px-5 py-3 border rounded bg-white row align-items-baseline">
+        <img src="../assets/img/doubleQuote.png" class="quote-art col-4" />
+        <div class="col-8 ">
+          <p class="mb-2">Like a friend sitting across the table; honest, straight-forward,
+            and lots of great info! I HIGHLY recommend.</p>
+          <h6 class="text-info mt-0 text-end">~ Summer D.</h6>
+        </div>
+      </div>
+
+    </div>
+    <!-- STUB 4 -->
+    <div class="col-10">
+      <div class="my-2 mx-5 px-5 py-3 border rounded bg-white row align-items-end">
+        <img src="../assets/img/doubleQuote.png" class="quote-art col-4" />
+        <div class="col-8 ">
+          <p class="mb-2">Whether you have sold on Etsy for 5 years or are just getting started,
+            this is like getting a mini degree in Etsy.</p>
+          <h6 class="text-info mt-0">~ Elyse G.</h6>
+        </div>
+      </div>
+
+    </div>
+    <!-- STUB 5 -->
+    <div class="col-10">
+      <div class="my-2 mx-5 px-5 py-3 border rounded bg-white row align-items-end">
+        <img src="../assets/img/doubleQuote.png" class="quote-art col-4" />
+        <div class="col-8 ">
+          <p class="mb-2">My shop improved immediately.A highly recommended resource!</p>
+          <h6 class="text-info mt-0">~ WoodenGiftCo</h6>
+        </div>
+      </div>
+
+    </div>
+    <div class="col-12">
+      <div class="m-5 px-5 py-3 border rounded bg-white row align-items-end">
+        <img src="../assets/img/doubleQuote.png" class="quote-art col-4" />
+        <div class="col-8 ">
+          <p class="mb-2">My shop improved immediately.A highly recommended resource!</p>
+          <h6 class="text-info mt-0">~ WoodenGiftCo</h6>
+        </div>
+      </div>
+
+    </div>
+
   </section>
 </template>
 
@@ -80,6 +145,7 @@ export default {
 
 .quote-art {
   height: 80px;
-  position: absolute;
+  width: 120px;
+
 }
 </style>
